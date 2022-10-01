e25b7f52850c8d4558a7ba13a631b27f3040f387
Edgar Alfonso Arvayo Ruiz <edgar.ruiz.arvayo@gmail.com>
2022-09-30 20:18:32 -0700

    Minor changes to the backend

--- server/src/middlewares/userExtractor.js
+++ server/src/middlewares/userExtractor.js
@@ -1,43 +1,43 @@
-import { Router } from "express"
+import { Router } from "express";
 
 //KEY
 import { SECRET } from "../settings/llaves";
 
 //Libreria requerida para autenticar
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 var verify = Router;
 
-verify = (req, res, next) =>{
-    let token = req.headers['x-access-token'] || req.headers['authorization'];
-    if(!token){
-        res.status(500).json({
-            valido: false,
-            mensaje: "Favor de introducir el token."
-        })
-        return
-    }   
-    if(token.startsWith('Bearer ')){
-        token = token.slice(7, token.length);
-        console.log(token);
-    }
-    if(token){
-        jwt.verify(token, SECRET, (err, decoded)=>{
-            if(err){
-                return res.json({
-                    valido: false,
-                    mensaje: "Token expirado o invalido."
-                });
-            }else{
-                req.decoded = ({
-                    valido: true,
-                    mensaje: "Token valido.",
-                    id: decoded.id
-                });
-                next();
-            }
-        })
-    }
-}
+verify = (req, res, next) => {
+   let token = req.headers["x-access-token"] || req.headers["authorization"];
+   if (!token) {
+      res.status(500).json({
+         valido: false,
+         mensaje: "Favor de introducir el token.",
+      });
+      return;
+   }
+   if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length);
+      console.log(token);
+   }
+   if (token) {
+      jwt.verify(token, SECRET, (err, decoded) => {
+         if (err) {
+            return res.json({
+               valido: false,
+               mensaje: "Token expirado o invalido.",
+            });
+         } else {
+            console.log(decoded);
+            req.decoded = {
+               valido: true,
+               mensaje: "Token valido.",
+            };
+            next();
+         }
+      });
+   }
+};
 
 export default verify;
